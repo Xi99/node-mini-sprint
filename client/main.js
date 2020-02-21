1798a73ad12bc0f60e3b7389f2b68f89b2b04b21
Hack Reactor Student <student@hackreactor.com>
2020-02-21 13:36:49 -0800

    Code getQuote

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  const PORT = 3000;
 // get a quote from the server when the page loads and add it to the dom
   getQuote();
 
@@ -10,10 +11,21 @@ $(document).ready(function() {
     addQuote(quote);
   });
 
-  function getQuote(){
+  function getQuote(successCB, errorCB = null) {
 
     //YOUR CODE HERE, Add a GET request
-
+    $.ajax({
+      url: `http://localhost:${PORT}/quote`,
+      type: 'GET',
+      data: { order: '-createdAt' },
+      contentType: 'application/json',
+      success: successCB || function (data) {
+        console.log('Random Quote Generator: Message sent');
+      },
+      error: errorCB || function(error) {
+        console.error('Random Quote Generator:  Failed to fetch messages', error);
+      }
+    });
   }
 
   function addQuote(quote){
@@ -22,3 +34,30 @@ $(document).ready(function() {
 
   }
 });
+
+
+
+//=========================original code
+
+// $(document).ready(function() {
+
+//   // get a quote from the server when the page loads and add it to the dom
+//     getQuote();
+  
+//   // when the user enters data and clicks submit, post the quote to the server
+//     $('#submit').click((e) => {
+//       e.preventDefault();
+//       let quote = $('input').val();
+//       addQuote(quote);
+//     });
+  
+//     function getQuote(successCB, errorCB = null) {
+  
+//       //YOUR CODE HERE, Add a GET request
+  
+//     function addQuote(quote){
+      
+//       //YOUR CODE HERE, Add a POST request
+  
+//     }
+//   });
